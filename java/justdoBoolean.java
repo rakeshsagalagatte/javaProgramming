3c617614fdae16350d10637af638ce725da6c587
ise <ise@ise-ThinkCentre-M73.(none)>
2019-08-09 11:50:49 +0530

    Fist

---
+++ java/justdoBoolean.java
@@ -0,0 +1,15 @@
+//import samp.d2ndlargest;
+//import java.util.Scanner;
+
+class BooleanDemo{
+  public static void main(String args[])
+  {
+    int x=12,y=2347;
+    System.out.println("X is greater than Y ? " + (x>y));
+    System.out.println("X is lessthan Y ? " + (x<y));
+    System.out.println("X is equal to Y ? " + (x==y));
+    boolean b = x>y;
+    System.out.println(b); 
+    
+  }
+}
