43cd7a1c905cb32d511563f2073a714bce82040f
Rakesh S R <rakeshsagalagatte@gmail.com>
2019-09-09 11:35:13 +0530

    Labset 4

---
+++ Labset/EmployeeNonstatic.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+class EmployeeDetails{
+
+   String emp_Name,designation;
+   int emp_ID,age;
+   double salary;
+   Scanner Su = new Scanner(System.in);
+   class EmployeeSalary{
+     double basic_sal,da,hra;
+     Scanner S = new Scanner(System.in);
+    
+     void readDetailsAndSalary()
+     {
+       System.out.print("Enter the Employee name :");
+       emp_Name = S.next();
+       System.out.print("Enter the Employee ID : ");
+       emp_ID = S.nextInt();
+       System.out.print("Enter the Employee age :");
+       age = S.nextInt();
+       System.out.print("Enter the Employee Designation :");
+       designation = S.next(); //POST of an employee
+       System.out.print("Enter the Employee basic salary :");
+       basic_sal = S.nextDouble();
+       System.out.print("Enter the Employee da :");  
+       da = S.nextDouble();
+       System.out.print("Enter the Employee hra :");
+       hra = S.nextDouble();
+       salary = hra + basic_sal + da + hra;
+       
+     }
+     
+     void display()
+     {
+       System.out.println(emp_Name + "       " + emp_ID + "        " + age + "           "+ designation + "             " + salary );
+     }
+   }
+   
+   void read()
+   {
+     System.out.println("Enter the number of employee : ");
+     int n = Su.nextInt();
+     EmployeeSalary[] e = new EmployeeSalary[100];
+     for(int i=0;i<n;i++)
+     {
+        System.out.println("Enter the employee number " + (i+1) +"detailes. ");
+        e[i] = new EmployeeSalary();
+        e[i].readDetailsAndSalary();
+     }
+     System.out.println("emp_Name          emp_ID      age        designation       salary ");
+     for(int i=0;i<n;i++)
+     {
+       e[i].display();
+     }
+     
+   }
+   
+  
+}
+
+public class EmployeeNonstatic{
+   public static void main(String args[])
+   {
+      System.out.println("Enter the Details of the employees.");
+      EmployeeDetails outer =  new EmployeeDetails();
+      outer.read();
+     
+   }
+}
--- Labset/StaticDemonstration.java
+++ Labset/StaticDemonstration.java
@@ -3,40 +3,43 @@ import java.util.Scanner;
 class Outer{
 
   static int a;
-   int c=34;
-static class Inner_Static{
-   int b=63;
-    void DisplayMethod()
-   {  a=10;
-     System.out.println("You are in static class and non-static method."+ a);
+          int c=34;
+  static class Inner_Static{
+          int b=63;
+          void Display()
+          { 
+            a=10;
+           System.out.println("You are in static class and non-static method. static a = "+ a + "  and static class non static data member non b = " + b);
+          }
    }
-  }
   
-  class Inner_NonStatic{
+          class Inner_NonStatic{
     
-   void DisplayMethod()
-   {
-      System.out.println("You are in non-static Inner class  ");
-   }
-  }
-  static void Static_Method()
-  {
-  System.out.println("You are in static class and a = ." + a );
-  }
+             void DisplayMethod()
+             {
+                 System.out.println("You are in non-static Inner class  ");
+                 System.out.println("non static outer class member c = " + c);
+             }
+           }
+            static void static_Method()
+           {
+             System.out.println("You are in static class and a = " + a );
+             
+           }
 }
 
-class MainClass{
+public class StaticDemonstration{
  
   
  public static void main(String args[])
- {    
+ {  
    Outer.Inner_Static J  = new Outer.Inner_Static();   //Declarartion of static class object;
-   J.DisplayMethod();
+   J.Display();
    Outer O = new Outer();                              //Declarartion of outer class object;
-   System.out.println("Static variable : " + O.a);    
-   O.Static_Method();
-  Outer.Inner_NonStatic Inn = new O.Inner_NonStatic();  //Declarartion of non-static inner class object;
-   
+     
+   O.static_Method();  
+   Outer.Inner_NonStatic Inn = new Inner_NonStatic();  //Declarartion of non-static inner class object;
+   Inn.DisplayMethod();
  }
 }
 
