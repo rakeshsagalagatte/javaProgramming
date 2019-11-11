2724cc70bb5ce1733171d2ead91d62ac03526f17
Rakesh S R <rakeshsagalagatte@gmail.com>
2019-11-11 13:16:08 +0530

    XML

---
+++ Labset/Part_B/LastAccessedHttp.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.text.DateFormat;
+
+public class LastAccessedHttp extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,java.io.IOException {
+     
+          response.setContentType("text/html");
+          java.io.PrintWriter out = response.getWriter();
+          HttpSession session = request.getSession();
+          Date creationTime = new Date(session.getCreationTime());
+          Date lastAccessed = new Date(session.getLastAccessedTime());
+          Date now = new Date();
+          DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.MEDIUM,DateFormat.MEDIUM);
+          out.println("<html>");
+          out.println("<head>");
+          out.println("<title> Displaying the Session Creation and Last-Accessed Time </title>");
+          out.println("</head>");
+          out.println("<body>");
+          out.println("<h2> Session Creation and Last-Accessed Time </h2> ");
+          out.println("The time and date now is : " + formatter.format(now) + "<br><br>");
+          out.println("<The session creation time: HttpSession.getCreationTime():" + formatter.format(creationTime)+ "<br><br>");
+          out.println("The last time the session was accessed: HttpSession.getLastAccessedTime():" + formatter.format(lastAccessed) );
+          out.println("</body>");
+          out.println("</html>");
+    }
+} 
